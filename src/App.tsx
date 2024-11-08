a760fe2f7de01969cd6224b4f5ddd4e3cfd38fd9
sybiriak <siibiryak@gmail.com>
2024-11-08 18:07:29 +0200

    Add solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,118 @@
 import './App.scss';
+import { TodoList } from './components/TodoList';
 
-// import usersFromServer from './api/users';
-// import todosFromServer from './api/todos';
+import usersFromServer from './api/users';
+import todosFromServer from './api/todos';
+import { useState } from 'react';
+import { Todo, User } from './types';
+
+function getNewId(todos: Todo[]): number {
+  return Math.max(...todos.map(todo => todo.id)) + 1;
+}
+
+function getUser(id: number): User | undefined {
+  return usersFromServer.find(user => user.id === id);
+}
+
+const baseTodos: Todo[] = todosFromServer.map(todo => ({
+  ...todo,
+  user: getUser(todo.userId),
+}));
+
+const defaultFormValues = {
+  title: '',
+  userId: 0,
+};
+
+type FormValues = typeof defaultFormValues;
+type FormErrors = Partial<Record<keyof FormValues, boolean>>;
 
 export const App = () => {
+  const [todos, setTodos] = useState<Todo[]>(baseTodos);
+  const [formValues, setFormValues] = useState<FormValues>(defaultFormValues);
+  const [formErrors, setFormErrors] = useState<FormErrors>({});
+
+  function handleChange(
+    event: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>,
+  ) {
+    const { name, value } = event.target;
+
+    setFormValues(currentValues => ({
+      ...currentValues,
+      [name]: value,
+    }));
+    setFormErrors({ ...formErrors, [name]: false });
+  }
+
+  function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+
+    const userId = +formValues.userId;
+    const title = formValues.title.trim();
+
+    if (!userId || !title) {
+      setFormErrors({
+        title: !title,
+        userId: !userId,
+      });
+
+      return;
+    }
+
+    setTodos([
+      ...todos,
+      {
+        id: getNewId(todos),
+        title,
+        completed: false,
+        userId,
+        user: getUser(userId),
+      },
+    ]);
+
+    setFormValues(defaultFormValues);
+    setFormErrors({});
+  }
+
   return (
     <div className="App">
       <h1>Add todo form</h1>
 
-      <form action="/api/todos" method="POST">
+      <form action="/api/todos" method="POST" onSubmit={handleSubmit}>
         <div className="field">
-          <input type="text" data-cy="titleInput" />
-          <span className="error">Please enter a title</span>
+          <input
+            name="title"
+            type="text"
+            data-cy="titleInput"
+            placeholder="Enter a title"
+            value={formValues.title}
+            onChange={handleChange}
+          />
+          {formErrors.title && (
+            <span className="error">Please enter a title</span>
+          )}
         </div>
 
         <div className="field">
-          <select data-cy="userSelect">
+          <select
+            name="userId"
+            data-cy="userSelect"
+            value={formValues.userId}
+            onChange={handleChange}
+          >
             <option value="0" disabled>
               Choose a user
             </option>
+            {usersFromServer.map(user => (
+              <option key={user.id} value={user.id}>
+                {user.name}
+              </option>
+            ))}
           </select>
 
-          <span className="error">Please choose a user</span>
+          {formErrors.userId && (
+            <span className="error">Please choose a user</span>
+          )}
         </div>
 
         <button type="submit" data-cy="submitButton">
@@ -29,33 +120,7 @@ export const App = () => {
         </button>
       </form>
 
-      <section className="TodoList">
-        <article data-id="1" className="TodoInfo TodoInfo--completed">
-          <h2 className="TodoInfo__title">delectus aut autem</h2>
-
-          <a className="UserInfo" href="mailto:Sincere@april.biz">
-            Leanne Graham
-          </a>
-        </article>
-
-        <article data-id="15" className="TodoInfo TodoInfo--completed">
-          <h2 className="TodoInfo__title">delectus aut autem</h2>
-
-          <a className="UserInfo" href="mailto:Sincere@april.biz">
-            Leanne Graham
-          </a>
-        </article>
-
-        <article data-id="2" className="TodoInfo">
-          <h2 className="TodoInfo__title">
-            quis ut nam facilis et officia qui
-          </h2>
-
-          <a className="UserInfo" href="mailto:Julianne.OConner@kory.org">
-            Patricia Lebsack
-          </a>
-        </article>
-      </section>
+      <TodoList todos={todos} />
     </div>
   );
 };
